3bef99b2a443c4673c3aa92c72b9c4dbb680a735
root <root@dep24073.uniwa.uwa.edu.au>
2023-06-19 18:23:22 +0800

    update

--- semantic_parser/models/unified/finetune.py
+++ semantic_parser/models/unified/finetune.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import torch
 from torch import nn
 from .base import PushToHubFriendlyModel
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
@@ -10,6 +10,7 @@ class Model(PushToHubFriendlyModel):
     def __init__(self, args):
         super().__init__()
         self.args = args
+        self.mid_dim = args.fine_tuning.mid_dim
 
         # Load tokenizer and model.
         self.tokenizer = AutoTokenizer.from_pretrained(args.bert.location, use_fast=False)
@@ -22,14 +23,44 @@ class Model(PushToHubFriendlyModel):
             self.tokenizer.add_tokens([v for k, v in args.special_tokens])
             self.pretrain_model.resize_token_embeddings(len(self.tokenizer))
 
-    def forward(self, input_ids, attention_mask, labels):
-        loss = self.pretrain_model(
+        # ZZHAO: Comp_GCN leverage
+        if self.args.model.use_pretrained_gcn:
+            self.gcn_emb_trans = nn.Sequential(
+                nn.Linear(self.args.model.gcn_dim, self.mid_dim//2),
+                nn.Tanh(),
+                nn.Linear(self.mid_dim//2, self.mid_dim),
+            )
+
+
+    def forward(self, input_ids, attention_mask, labels, **kwargs):
+        out = self.pretrain_model(
             input_ids=input_ids,
             attention_mask=attention_mask,
             use_cache=False,
             labels=labels,
-        ).loss
-        return {'loss': loss}
+        )
+        encoder_last_hidden_state = out.encoder_last_hidden_state  #[2, 512, 768]
+        if self.args.model.use_pretrained_gcn and  'tag_embeddings' in kwargs:
+            tag_gcn_emb = self.gcn_emb_trans(kwargs['tag_embeddings']) #[2, 90, 512]
+            prop_gcn_emb = self.gcn_emb_trans(kwargs['property_embeddings'])
+            ent_gcn_emb = torch.einsum('ijk, ijk->ik', tag_gcn_emb, prop_gcn_emb) #[2, 512]
+
+            entity_gcn_emb= torch.unsqueeze(ent_gcn_emb, -1)  #[2, 512, 1]
+            aligned_entity_gcn_emb= nn.functional.interpolate(
+                entity_gcn_emb, size=encoder_last_hidden_state.size()[2:], mode='linear', align_corners=False
+            )#[2, 512, 768])
+
+            encoder_outputs= tuple([torch.einsum('bij,bij->bij', encoder_last_hidden_state, aligned_entity_gcn_emb) for _ in range(self.match_n_layer)]) # batch element-wise product 
+
+            out_ = self.pretrain_model(input_ids=input_ids, 
+                                        attention_mask=attention_mask,
+                                        encoder_outputs=encoder_outputs,
+                                        labels = labels,
+                                        past_prompt=past_prompt) # logits shape: [2, 521, 32103]
+            print(f'T5 loss: {out.loss}, leveraged_model loss output: {out_.loss}')
+
+            return {'loss': out_.loss}
+        return {'loss': out.loss}
 
     def generate(self, input_ids, attention_mask, **kwargs):
         generated_ids = self.pretrain_model.generate(
