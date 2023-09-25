f1994472e8ce8d6ffcc5de552c372a5c7825e003
22842219 <57472298+22842219@users.noreply.github.com>
2023-09-25 13:47:09 +0800

    Update schema2graph.py

--- application/rel_db2kg/schema2graph.py
+++ application/rel_db2kg/schema2graph.py
@@ -520,15 +520,12 @@ class RelDB2KGraphBuilder(RelDBDataset):
             self.table2edges(db_name, tbs_dict)
             self.build_edges(db_name, tbs_dict) # this process should run based on table2node process
         
-def create_edge(env_file, db_id, this_tb, ref_tb, this_fk, ref_fk, fk_value):
-    env = Env()
-    env.read_env(env_file)
-    graph = Graph(password=env("GRAPH_PASSWORD"))
-
+def create_edge(graph, db_id, this_tb, ref_tb, this_fk, ref_fk, fk_value):
     cypher = 'MATCH (m:`{0}.{1}` {{{3}: {5}}}),(n:`{0}.{2}` {{{4}:{5}}}) \
                 CREATE (m)-[:`{0}.{1}_HAS_{0}.{2}`]->(n);'.format(db_id, this_tb, ref_tb, this_fk, ref_fk, fk_value)
     print(cypher)
     graph.run(cypher)
+
 
 def main():
     import json
@@ -571,38 +568,39 @@ def main():
         raise NotImplementedError
     
     if args.create:
-       # To advoid Java heap space issue, in each relational database, 
-       # we retrive the relational databases to get the corresponding rows aligned with the foreign key constraints.
-       db_info_fph = os.path.join(root, 'application', 'rel_db2kg', 'data', benchmark,  'KaggleDBQA_tables.json')
-       with open(db_info_fph, 'r') as f:
-        db_info = json.load(f)
-        for db in db_info:
-            db_id = db.get('db_id')
-            fks = db.get('foreign_keys')
-            # pks = db.get('primary_keys')
-            tbs = db.get('table_names_original')
-            cols = db.get('column_names_original')
-            cols_type = db.get('column_types')
-            if fks:
-                for pair in fks:
-                    this = cols[pair[0]] # [tb_id, col_name]
-                    ref = cols[pair[1]]
-                    this_tb = tbs[this[0]]
-                    ref_tb = tbs[ref[0]]
-                    this_fk = this[1]
-                    ref_fk =ref[1]
-                    this_fk_type = cols_type[pair[0]]
-                    ref_fk_type = cols_type[pair[1]]
-                    
-                    fdb = os.path.join(root, 'application', 'rel_db2kg', 'data', benchmark, 'database', db_id, '{}.sqlite'.format(db_id))
-                    engine = DBengine(fdb)
-                    fk_values = engine.get_fk_values( this_fk, ref_fk, this_tb, ref_tb)
-                    for fk_value in fk_values:
-                        if this_fk_type=='text' or ref_fk_type=='text':
-                            fk_value="'{}'".format(fk_value)
-                        create_edge(env_file, db_id, this_tb, ref_tb, this_fk, ref_fk, fk_value)
+        # To advoid Java heap space issue, in each relational database,
+        # we retrive the relational databases to get the corresponding rows aligned with the foreign key constraints.
+        db_info_fph = os.path.join(root, 'application', 'rel_db2kg', 'data', benchmark,  'KaggleDBQA_tables.json')
+        env = Env()
+        env.read_env(env_file)
+        graph = Graph(password=env("GRAPH_PASSWORD"))
 
+        with open(db_info_fph, 'r') as f:
+            db_info = json.load(f)
+            for db in db_info:
+                db_id = db.get('db_id')
+                fdb = os.path.join(root, 'application', 'rel_db2kg', 'data', benchmark, 'database', db_id, '{}.sqlite'.format(db_id))
+                engine = DBengine(fdb)
+                fks = db.get('foreign_keys')
+                # pks = db.get('primary_keys')
+                tbs = db.get('table_names_original')
+                cols = db.get('column_names_original')
 
+                if fks:
+                    for pair in fks:
+                        this = cols[pair[0]] # [tb_id, col_name]
+                        ref = cols[pair[1]]
+                        this_tb = tbs[this[0]]
+                        ref_tb = tbs[ref[0]]
+                        this_fk = this[1]
+                        ref_fk =ref[1]
+                        fk_values = engine.get_fk_values( this_fk, ref_fk, this_tb, ref_tb)
+                        print(db_id, this_tb, ref_tb, this_fk, ref_fk)
+                        for fk_value in fk_values:
+                            if isinstance(fk_value, str):
+                                fk_value = "'{}'".format((fk_value.strip("'").strip('"')))
+                            create_edge(graph, db_id, this_tb, ref_tb, this_fk, ref_fk, fk_value)
+    
 
 
 if __name__ == "__main__":
