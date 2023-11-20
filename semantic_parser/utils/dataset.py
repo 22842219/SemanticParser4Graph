be589e477570778db95515451988828914d469c9
22842219 <57472298+22842219@users.noreply.github.com>
2023-11-20 15:52:56 +0800

    Update dataset.py

--- semantic_parser/utils/dataset.py
+++ semantic_parser/utils/dataset.py
@@ -14,11 +14,13 @@ class TokenizedDataset(Dataset):
 
         self.conv_sep = " || "
 
-        #ZZHAO: get the maximum number of properties in a db for the tensor padding. 
-        db_tag_props_num = []
+        #ZZHAO: get the maximum number of triplets in a db for the tensor padding. 
+        db_trip_num = []
         for idx in range(len(self.seq2seq_dataset)):
-             db_tag_props_num.append(len(self.seq2seq_dataset[idx]['db_property_embs']['property_embeddings'] ))
-        self.max_ent_length = max(db_tag_props_num)
+             db_trip_num.append(len(self.seq2seq_dataset[idx]['schema_subword_ids']))
+        self.max_trip_length = max(db_trip_num)
+        
+  
 
     def __getitem__(self, index):
         
@@ -37,18 +39,20 @@ class TokenizedDataset(Dataset):
                 if self.args.model.knowledge_usage == 'concatenate' or self.args.model.knowledge_usage is None:
                     # seq_in  = "[utt n] ; structured knowledge: struct_in ; context: [utt n-1] | [utt n-2] | ..."
                     seq_in = "{} ; structured knowledge: {} ; context: {}".format(raw_item["text_in"][:index],
-                                                                                  raw_item["struct_in"],
-                                                                                  raw_item["text_in"][index + len(self.conv_sep):])
+                                                                                raw_item["struct_in"],
+                                                                                raw_item["text_in"][index + len(self.conv_sep):])
                 elif self.args.model.knowledge_usage == 'separate':
                     # seq_in  = "[utt n] ; context: [utt n-1] | [utt n-2] | ..."
                     seq_in = "{} ; context: {}".format(raw_item["text_in"][:index],
-                                                       raw_item["text_in"][index + len(self.conv_sep):])
+                                                    raw_item["text_in"][index + len(self.conv_sep):])
                 else:
                     raise ValueError()
+    
             else:
                 ######################
                 # Non-conversational #
                 ######################
+
                 if self.args.model.knowledge_usage == 'concatenate' or self.args.model.knowledge_usage is None:
                     # seq_in  = "text_in ; structured knowledge: struct_in"
                     seq_in = "{} ; structured knowledge: {}".format(raw_item["text_in"], raw_item["struct_in"])
@@ -57,6 +61,7 @@ class TokenizedDataset(Dataset):
                     seq_in = raw_item["text_in"]
                 else:
                     raise ValueError()
+     
         else:
             ######################
             # Without text input #
@@ -84,6 +89,7 @@ class TokenizedDataset(Dataset):
             # assign the max input length into some large numbers, instead of using the "max_model_length"
             # ,which the default is 512, which will hurt the performance a lot.
         )
+        
         tokenized_inferred = self.tokenizer(
             raw_item["seq_out"],
             padding="max_length",
@@ -124,26 +130,19 @@ class TokenizedDataset(Dataset):
                                                  )
             item['knowledge_input_ids'] = torch.LongTensor(tokenized_knowledge.data["input_ids"])
             item['knowledge_attention_mask'] = torch.LongTensor(tokenized_knowledge.data["attention_mask"])
-        
-        #TODO: Use pretrained compositional gcn embedding. BY ZZHAO
-        if self.args.model.use_pretrained_gcn:
-            if 'db_property_embs' in raw_item:
-                tag_embeddings= torch.tensor(raw_item['db_property_embs']['tag_embeddings']) #torch.Size([13, 200]) 
-                property_embeddings = torch.tensor(raw_item['db_property_embs']['property_embeddings'] ) #torch.Size([13, 200]) 
     
+       
+        # item['schema_knowledge']=raw_item['schema_knowledge']
+        if 'schema_subword_ids'  in raw_item:
+            schema_subword_ids=torch.tensor(raw_item['schema_subword_ids']) # [num_triplets, 300]
+            padding_length = self.max_trip_length - len(schema_subword_ids)
+            item['schema_subword_ids'] = F.pad(schema_subword_ids, (0, 0, 0, padding_length))
+        # if 'schema_schema_embs'  in raw_item:
+        #     schema_subword_embs=torch.tensor(raw_item['schema_schema_embs']) # [num_triplets, 300]
+        #     embs_padding_length = self.max_trip_length - len(schema_subword_embs)
+        #     item['schema_schema_embs'] = F.pad(schema_subword_embs, (0, 0, 0, embs_padding_length))    
+        #     # print('schema_subword_ids in datasets:', item['schema_subword_ids'].shape)
 
-                # Pad tag_embeddings and property_embeddings to match max_ent_length
-                num_entities = len(tag_embeddings)
-                padding_length = self.max_ent_length - num_entities
-                padded_tag_embeddings = F.pad(tag_embeddings, (0, 0, 0, padding_length))
-                padded_property_embeddings = F.pad(property_embeddings, (0, 0, 0, padding_length))
-
-                item['tag_embeddings'] = padded_tag_embeddings
-                item['property_embeddings'] = padded_property_embeddings
-            # print(self.max_ent_length)
-            # print(item['tag_embeddings'].shape, item['property_embeddings'].shape)
-            # print(item['tag_embeddings'])
-            # assert 1>2
         return item
 
     def __len__(self):
